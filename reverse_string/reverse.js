6fe32714a2f4b1a664f5110ffdb47a4148acc0c5
developerk786 <designersohail786@gmail.com>
2018-04-03 17:51:41 +0545

    Initial commit

--- reverse_string/reverse.js
+++ reverse_string/reverse.js
@@ -1,21 +1,22 @@
-// Method: 1 
-function reverse_string(str) {
-  return str
-    .split('')
-    .reverse()
-    .join('')
-}
+// Method: 1
+// function reverse_string(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('')
+// }
 
 // Method: 2
-function reverse_string(str) {
-  let reversed = ''
-  for (let char of str) {
-    reversed = char + reversed
-  }
-  return reversed
-}
+// function reverse_string(str) {
+//   let reversed = ''
+//   for (let char of str) {
+//     reversed = char + reversed
+//   }
+//   return reversed
+// }
+
 
 // Method: 3
-function reverse_string(str) {
-  
-}
+// function reverse_string(str) {
+//
+// }
